bee6e8197210336a5bb2222d9925fddf89670e3d
Nora Asverta <sorarasverta@gmail.com>
2025-08-25 10:37:26 +0700

    feat: complete sprint 0 setup with dependencies and firebase

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+import 'firebase_options.dart'; // File ini otomatis dibuat oleh flutterfire_cli
+
+void main() async {
+  // Pastikan semuanya siap sebelum menjalankan app
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inisialisasi Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Catatan AI",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    );
+  }
+}
