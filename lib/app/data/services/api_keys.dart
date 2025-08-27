88916a12d115b8abef10d302405190ed9bd542ed
Nora Asverta <sorarasverta@gmail.com>
2025-08-27 11:06:58 +0700

    feat: API

---
+++ lib/app/data/services/api_keys.dart
@@ -0,0 +1,5 @@
+// File ini berisi semua kunci API rahasia.
+// PENTING: File ini TIDAK BOLEH di-commit ke Git.
+// Pastikan path file ini sudah ditambahkan ke dalam .gitignore
+
+const String geminiApiKey = 'AIzaSyCcm5gwacHQcco5oMwJU32IJ2MR2znGCoI';
